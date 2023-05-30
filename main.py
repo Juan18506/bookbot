777b9b08badef62a0ab9ca143bc6abd786d1893d
Juan18506 <juan185067@gmail.com>
2023-05-29 21:18:58 -0300

    added count chars feature

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 def get_book_text(path: str) -> str:
     with open(path) as f:
         return f.read()
-    
-def count_words(text: str) -> int:
+
+
+def get_num_words(text: str) -> int:
     words = text.split()
     return len(words)
+
+
+def get_chars_dict(text: str) -> int:
+    text = text.lower()
+    chars = {}
+    for char in text:
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
+
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    number_of_words = count_words(text)
-    print(f"Word count: {number_of_words}")
-    
+    chars_dict = get_chars_dict(text)
+    print(f"Chars dict: {chars_dict}")
+
+
 main()
